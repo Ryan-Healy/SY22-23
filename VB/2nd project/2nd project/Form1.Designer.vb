73761ac1f4ccfc8d52d740a26d9a080d6577ba38
Ryan <rphea713@learner.dvusd.org>
2022-08-17 08:26:23 -0700

    working version

---
+++ VB/2nd project/2nd project/Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.KobePictureBox = New System.Windows.Forms.PictureBox()
+        Me.MagicPictureBox = New System.Windows.Forms.PictureBox()
+        Me.MJPictureBox = New System.Windows.Forms.PictureBox()
+        Me.LebronPictureBox = New System.Windows.Forms.PictureBox()
+        Me.KareemPictureBox = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Displaypicturebox = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.KobePictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MagicPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.MJPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LebronPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.KareemPictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Displaypicturebox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(53, 339)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "MJ"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'KobePictureBox
+        '
+        Me.KobePictureBox.Image = Global._2nd_project.My.Resources.Resources.kobe
+        Me.KobePictureBox.Location = New System.Drawing.Point(464, 388)
+        Me.KobePictureBox.Name = "KobePictureBox"
+        Me.KobePictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.KobePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.KobePictureBox.TabIndex = 5
+        Me.KobePictureBox.TabStop = False
+        '
+        'MagicPictureBox
+        '
+        Me.MagicPictureBox.Image = Global._2nd_project.My.Resources.Resources.magic
+        Me.MagicPictureBox.Location = New System.Drawing.Point(358, 388)
+        Me.MagicPictureBox.Name = "MagicPictureBox"
+        Me.MagicPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.MagicPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.MagicPictureBox.TabIndex = 4
+        Me.MagicPictureBox.TabStop = False
+        '
+        'MJPictureBox
+        '
+        Me.MJPictureBox.Image = Global._2nd_project.My.Resources.Resources.MJ
+        Me.MJPictureBox.Location = New System.Drawing.Point(40, 388)
+        Me.MJPictureBox.Name = "MJPictureBox"
+        Me.MJPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.MJPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.MJPictureBox.TabIndex = 3
+        Me.MJPictureBox.TabStop = False
+        '
+        'LebronPictureBox
+        '
+        Me.LebronPictureBox.Image = Global._2nd_project.My.Resources.Resources.lebron
+        Me.LebronPictureBox.Location = New System.Drawing.Point(146, 388)
+        Me.LebronPictureBox.Name = "LebronPictureBox"
+        Me.LebronPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.LebronPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.LebronPictureBox.TabIndex = 2
+        Me.LebronPictureBox.TabStop = False
+        '
+        'KareemPictureBox
+        '
+        Me.KareemPictureBox.Image = Global._2nd_project.My.Resources.Resources.kareem
+        Me.KareemPictureBox.Location = New System.Drawing.Point(252, 388)
+        Me.KareemPictureBox.Name = "KareemPictureBox"
+        Me.KareemPictureBox.Size = New System.Drawing.Size(100, 50)
+        Me.KareemPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.KareemPictureBox.TabIndex = 1
+        Me.KareemPictureBox.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(156, 339)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "lebron"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(265, 339)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "kareem"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(368, 339)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "magic"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(464, 339)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "kobe"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Displaypicturebox
+        '
+        Me.Displaypicturebox.Location = New System.Drawing.Point(0, 1)
+        Me.Displaypicturebox.Name = "Displaypicturebox"
+        Me.Displaypicturebox.Size = New System.Drawing.Size(352, 208)
+        Me.Displaypicturebox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.Displaypicturebox.TabIndex = 10
+        Me.Displaypicturebox.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(392, 29)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 16)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(395, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(85, 16)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "vertical jump:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(395, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 16)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "height:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(395, 127)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(102, 16)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "signature move:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(461, 164)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(0, 16)
+        Me.Label5.TabIndex = 15
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(395, 164)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 16)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "speed:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1080, 450)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Displaypicturebox)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.KobePictureBox)
+        Me.Controls.Add(Me.MagicPictureBox)
+        Me.Controls.Add(Me.MJPictureBox)
+        Me.Controls.Add(Me.LebronPictureBox)
+        Me.Controls.Add(Me.KareemPictureBox)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.KobePictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MagicPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.MJPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LebronPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.KareemPictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Displaypicturebox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents KareemPictureBox As PictureBox
+    Friend WithEvents LebronPictureBox As PictureBox
+    Friend WithEvents MJPictureBox As PictureBox
+    Friend WithEvents MagicPictureBox As PictureBox
+    Friend WithEvents KobePictureBox As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Displaypicturebox As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ VB/2nd project/2nd project/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class Form1
+    Private Sub PictureBox3_Click(sender As Object, e As EventArgs) Handles MJPictureBox.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Displaypicturebox.Image = MJPictureBox.Image
+        Label1.Text = "Micheal Jordan"
+        Label2.Text = "During his vertical jump, his feet are 4 feet off the ground"
+        Label3.Text = "He is 6 foot 6 inches"
+        Label4.Text = "His signature move was his mastered fadeaway from the basket"
+        Label5.Text = "Micheal Jordans top speed was a 4.38 40 yard dash which is crazy fast"
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Displaypicturebox.Image = LebronPictureBox.Image
+        Label1.Text = "Lebron James"
+        Label2.Text = "His vertical jump is just above 40 inches or close to 4 feet "
+        Label3.Text = "He is 6 foot 8 inches"
+        Label4.Text = "His signature move is the Tomahawk dunk"
+        Label5.Text = "Lebron's top speed was a 4.4 40 yard dash"
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Displaypicturebox.Image = KareemPictureBox.Image
+        Label1.Text = "Kareem Abdul-Jebar"
+        Label2.Text = "He had a 44 inch vertical"
+        Label3.Text = "Kareem is 7 foot 2 inches"
+        Label4.Text = "His signature move was his famous Skyhook"
+        Label5.Text = "He never did a 40 yard dash so we dont know what it was"
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Displaypicturebox.Image = MagicPictureBox.Image
+        Label1.Text = "Magic Johnson"
+        Label2.Text = "His vertical was 30 inches"
+        Label3.Text = "He was 6 foot 9 inches"
+        Label4.Text = "He was known for his no look passes"
+        Label5.Text = "He never did a 40 yard dash"
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Displaypicturebox.Image = KobePictureBox.Image
+        Label1.Text = "Kobe Bryant"
+        Label2.Text = "His vertical was 38 inches"
+        Label3.Text = "He was 6 foot 6 inches"
+        Label4.Text = "His signature move was his mastered fadeaway"
+        Label5.Text = "He ran a 4.30 40 yard dash"
+    End Sub
+End Class
